d33181049095221dfc0363add32a39a4dc02a1ef
Tu Nombre de Usuario <tucorreo@ejemplo.com>
2026-07-29 21:38:47 -0500

    Update script for Pokemon 6

--- add_pokemon.py
+++ add_pokemon.py
@@ -19,25 +19,25 @@ def run():
             db.session.commit()
 
         # Buscar si la película ya existe
-        movie = Movie.query.filter(Movie.title.ilike('%Pokémon 5: Latios y Latias%')).first()
+        movie = Movie.query.filter(Movie.title.ilike('%Pokémon 6: Jirachi y los Deseos%')).first()
         
         if not movie:
-            print("Creando la película 'Pokémon 5: Latios y Latias' en la base de datos...")
+            print("Creando la película 'Pokémon 6: Jirachi y los Deseos' en la base de datos...")
             movie = Movie(
-                title='Pokémon 5: Latios y Latias',
-                slug='pokemon-latios-y-latias',
-                synopsis='Ash, Pikachu y sus amigos llegan a Altomare, la hermosa capital del agua. Allí conocen a Latios y Latias, dos misteriosos Pokémon legendarios que protegen la ciudad y la Joya del Alma. Cuando un par de famosas ladronas intentan robar la joya, desencadenan una catástrofe que amenaza con hundir la ciudad entera.',
-                year=2002,
-                video_url='https://ok.ru/videoembed/10214016944878',
+                title='Pokémon 6: Jirachi y los Deseos',
+                slug='pokemon-jirachi-y-los-deseos',
+                synopsis='Ash, Pikachu y sus amigos visitan un increíble festival itinerante por la llegada del Cometa Milenario. Allí descubren a Jirachi, un tierno y mítico Pokémon que despierta solo cada mil años para conceder deseos. Sin embargo, un malvado mago planea usar el poder de Jirachi para despertar a un ser destructivo. Ash deberá arriesgar todo para salvar a su nuevo amigo.',
+                year=2003,
+                video_url='https://ok.ru/videoembed/10213891443438',
                 is_active=True
             )
             movie.categories.append(category)
             db.session.add(movie)
             db.session.commit()
-            print("¡Película Pokémon 5 añadida con éxito!")
+            print("¡Película Pokémon 6 añadida con éxito!")
         else:
             print("La película ya existía, actualizando el link de video...")
-            movie.video_url = 'https://ok.ru/videoembed/10214016944878'
+            movie.video_url = 'https://ok.ru/videoembed/10213891443438'
             db.session.commit()
             print("¡Video actualizado con éxito!")
 
